8b72f95f1637bcee5411c17318c39158285e4666
Austin Blain and Annie Sonna <austin@>
2017-01-03 14:21:29 -0800

    Add the error message option as fail-save

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -2,12 +2,13 @@ var apiKey = "129327718f475acb9e16c2286c58b85c";
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
-    debugger;
     var city = $('#location').val();
-    $('#location').val();
+    $('#location').val("");
     $('.showWeather').text("The city you have chosen is " + city + ".");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' +apiKey, function(response) {
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
       $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    }).fail(function(error) {
+      $('.showWeather').text(error.responseJSON.message);
     });
   });
 });
